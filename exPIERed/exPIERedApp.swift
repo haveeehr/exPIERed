a720c9a9919e34c38427a7233b376c39ca46ca5b
Javier Fransiscus <javier.fransiscus@graveltechnology.com>
2023-01-11 11:10:38 +0100

    Add SwiftUI Project

---
+++ exPIERed/exPIERedApp.swift
@@ -0,0 +1,17 @@
+//
+//  exPIERedApp.swift
+//  exPIERed
+//
+//  Created by Javier Fransiscus on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct exPIERedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
