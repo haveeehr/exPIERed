d51cff9631f0f6c466685446310daed6d97bb646
haveeehr <pier9402@gmail.com>
2023-01-11 12:10:30 +0100

    Product Model

---
+++ exPIERed/Model/Product.swift
@@ -0,0 +1,50 @@
+//
+//  Product.swift
+//  exPIERed
+//
+//  Created by Javier Fransiscus on 11/01/23.
+//
+import Foundation
+
+struct Product: Identifiable {
+    let id = UUID()
+    var name: String
+    var dateCreated = Date()
+    var expiryDate: Date
+    var isOpened: Bool
+    var category: Category
+    
+    static var products: [Product] = [
+        Product(name: "Chicken AIA", expiryDate: Date(), isOpened: false, category: .meat),
+        Product(name: "Tacchino", expiryDate: Date(), isOpened: true, category: .meat),
+        Product(name: "Banana", expiryDate: Date(), isOpened: false, category: .fruit),
+        Product(name: "Insalata", expiryDate: Date(), isOpened: true, category: .vegetables)
+    ]
+}
+
+enum Category {
+    case meat
+    case fish
+    case vegetables
+    case fruit
+    case others
+    
+    var name: String {
+        switch self {
+        case .meat:
+            "Meat"
+        case .fish:
+            "Fish"
+        case .vegetables:
+            "Vegetables"
+        case .fruit:
+            "Fruit"
+        case .others:
+            "Others"
+        }
+    }
+}
+
+
+
+
