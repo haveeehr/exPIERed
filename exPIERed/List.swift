09e4bc6f965f176ce9c33d8ada90a06b1ee20457
VitoGallo <vito.gallo94@gmail.com>
2023-01-11 11:38:42 +0100

    Create a new branch.

---
+++ exPIERed/List.swift
@@ -0,0 +1,20 @@
+//
+//  List.swift
+//  exPIERed
+//
+//  Created by Vito Gallo on 11/01/23.
+//
+
+import SwiftUI
+
+struct List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct List_Previews: PreviewProvider {
+    static var previews: some View {
+        List()
+    }
+}
