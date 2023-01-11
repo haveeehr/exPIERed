71c53b64b6aafd4fdd5badafd81c078edfde18a2
SidusProxy <g.annina@studenti.unina.it>
2023-01-11 11:25:13 +0100

    Added the category view in a new branch

---
+++ exPIERed/CategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryView.swift
+//  exPIERed
+//
+//  Created by Gianluca Annina on 11/01/23.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text("Hello I am the category view fra")
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
